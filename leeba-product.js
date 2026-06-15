c07010363b2d9e2f9ca2bb24a78c4ef488fe2f75
Dipen Shah <dipenshah210@users.noreply.github.com>
2026-06-16 01:52:55 +0530

    Add files via upload

--- leeba-product.js
+++ leeba-product.js
@@ -5,6 +5,7 @@
 /* ─────────────────────────────────────────────────
    CONFIG
 ───────────────────────────────────────────────── */
+// var API_BASE = 'https://script.google.com/macros/s/AKfycbxrmx77TuZv1HrN_pz40zB4znh1nEX7Rn6Nurc184bP1jx3YcQ3vOcB--LQP3Mb3zh3CA/exec';
 var API_BASE = 'https://script.google.com/macros/s/AKfycbxIKjKGaz4h9LerAm7Vn81nd5AJiOGpxhLAl8V0vSCgymkleCiCm4qPyG1ZkfSXbdp7tw/exec';
 var S3_BASE  = 'https://leeba-media.s3.ap-south-1.amazonaws.com/';
 var WA_NUM   = '919979460555';
@@ -416,11 +417,12 @@ function initVideoPlayer() {
 
   /* Fullscreen */
   fsBtn.addEventListener('click', function() {
-    var el = frame;
-    if (document.fullscreenElement) {
-      document.exitFullscreen && document.exitFullscreen();
+    if (document.fullscreenElement || document.webkitFullscreenElement) {
+      (document.exitFullscreen || document.webkitExitFullscreen || function(){}).call(document);
+    } else if (vid.webkitEnterFullscreen) {
+      vid.webkitEnterFullscreen();
     } else {
-      (el.requestFullscreen || el.webkitRequestFullscreen || el.mozRequestFullScreen).call(el);
+      (frame.requestFullscreen || frame.webkitRequestFullscreen || function(){}).call(frame);
     }
     showControls();
   });
